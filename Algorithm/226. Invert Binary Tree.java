3af5ba97f3eed326f891692a370ed5e6859ea11c
luceneandjbm <39853194+luceneandjbm@users.noreply.github.com>
2018-08-23 09:51:13 +0800

    Update 226. Invert Binary Tree.java

--- Algorithm/226. Invert Binary Tree.java
+++ Algorithm/226. Invert Binary Tree.java
@@ -1,21 +1,3 @@
-翻转一棵二叉树。
-
-示例：
-
-输入：
-
-     4
-   /   \
-  2     7
- / \   / \
-1   3 6   9
-输出：
-
-     4
-   /   \
-  7     2
- / \   / \
-9   6 3   1
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
@@ -26,14 +8,18 @@
  * }
  */
 class Solution {
-    public TreeNode invertTree(TreeNode root) {
-		//是个后序遍历
-        if(root == null) return null;
-		TreeNode left = invertTree(root.left);
-		TreeNode right = invertTree(root.right);
-		//处理逻辑
+	public TreeNode robot(TreeNode root){
+		if(root == null) return null;
+		//反转左子树
+		TreeNode left = robot(root.left);
+		//反转右子树
+		TreeNode right = robot(root.right);
+		//交换左右子树
 		root.left = right;
 		root.right = left;
-		return root;
+        return root;
+	}
+    public TreeNode invertTree(TreeNode root) {
+		return robot(root);
     }
 }
