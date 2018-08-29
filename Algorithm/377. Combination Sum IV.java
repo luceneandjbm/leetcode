2434b92cd0803b08343dcb753edad8acdedd93f0
luceneandjbm <39853194+luceneandjbm@users.noreply.github.com>
2018-08-29 16:48:19 +0800

    Update 377. Combination Sum IV.java

--- Algorithm/377. Combination Sum IV.java
+++ Algorithm/377. Combination Sum IV.java
@@ -17,3 +17,19 @@ class Solution {
 		return robot(nums,target);
     }
 }
+
+使用dp
+class Solution {
+    public int combinationSum4(int[] nums, int target) {
+        int[] dp = new int[target + 1];
+		dp[0] = 1;
+		for(int i = 1; i < target + 1; i ++){
+			for(int j = 0; j < nums.length; j ++){
+				if(i - nums[j] >= 0){
+					dp[i] += dp[i-nums[j]];
+				}
+			}
+		}
+		return dp[target];
+    }
+}
